0faa1b544f4d0a96e860327d843dea2b9f2ee0c6
Brian Omondi <omondibryan1@gmail.com>
2025-07-17 19:29:37 +0300

    Create layout.tsx

---
+++ continue/src/app/layout.tsx
@@ -0,0 +1,11 @@
+import { EditorProvider } from "@/contexts/EditorContext";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <EditorProvider>{children}</EditorProvider>
+      </body>
+    </html>
+  );
+}
