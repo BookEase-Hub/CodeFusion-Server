b9f0d1cddb6f0ca17a473a1ea05373b6e5464691
Brian Omondi <omondibryan1@gmail.com>
2025-07-17 15:00:16 +0300

    Create AuthService.ts

---
+++ services/AuthService.ts
@@ -0,0 +1,13 @@
+import { User } from '../models/User';
+
+export class AuthService {
+  public async authenticate(token: string): Promise<User | null> {
+    // Logic to authenticate user
+    return null;
+  }
+
+  public async authorize(user: User, resource: string): Promise<boolean> {
+    // Logic to authorize user
+    return false;
+  }
+}
